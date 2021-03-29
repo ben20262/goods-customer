2b5b29bc1e6a8906d96e8bfbf87cfa0aa3a3d4c3
ben20262 <ben20262@aim.com>
2021-03-29 19:48:01 -0400

    component setup

---
+++ src/components/promotions/Promotion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class Promotion extends Component {
+    render() { 
+        return (
+            <div>
+                Promotion
+            </div>
+        );
+    }
+}
+ 
+export default Promotion;
---
+++ src/components/promotions/Promotions.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class Promotions extends Component {
+    render() { 
+        return (
+            <div>
+                Promotions
+            </div>
+        );
+    }
+}
+ 
+export default Promotions;
