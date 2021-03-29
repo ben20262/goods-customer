27af927a83d322daf70e75d03beecfd799b553aa
ben20262 <ben20262@aim.com>
2021-03-29 19:21:23 -0400

    added Categories

---
+++ src/containers/CategoriesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class CategoriesContainer extends Component {
+    render() { 
+        return (
+            <div>
+                CategoriesContainer
+            </div>
+        );
+    }
+}
+ 
+export default CategoriesContainer;
