c6e99391b6b49d3fac5cc28a1f122e63e2eb72ef
ben20262 <ben20262@aim.com>
2021-03-29 19:40:09 -0400

    added rootReducer funtionality

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,17 @@
-export default function rootReducer (state = {items: [], categories: []}) {
-    return state
+export default function rootReducer (state = {items: [], categories: [], loading: false}, action) {
+    switch (action.type) {
+        case 'LOADING ITEMS':
+            return {
+                ...state,
+                loading: true
+            }
+        case 'ADD_ITEMS':
+            return {
+                items: action.items,
+                categories: action.categories,
+                loading: false
+            }
+        default:
+            return state
+    }
 }
